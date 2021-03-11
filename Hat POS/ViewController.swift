b342f7e22831ab1687d1e94fbd5e4e75d6b0415a
Space-96 <brendanbernal@live.com>
2021-03-11 09:59:54 -0600

    Initial Commit

---
+++ Hat POS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hat POS
+//
+//  Created by Brendan on 3/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
