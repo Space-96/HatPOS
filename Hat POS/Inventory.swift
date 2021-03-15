b9e1f8de860947f3f152e1371c402b6b6bf5edbf
Space-96 <brendanbernal@live.com>
2021-03-15 15:31:16 -0500

    Pulls data from php into Xcode

---
+++ Hat POS/Inventory.swift
@@ -0,0 +1,19 @@
+//
+//  Inventory.swift
+//  Hat POS
+//
+//  Created by Brendan on 3/12/21.
+//
+
+import Foundation
+
+struct Inventory {
+    
+    var productID = ""
+    var productName = ""
+    var productDescription = ""
+    var unitPurchasePrice = ""
+    var unitSalePrice = ""
+    var quantity = ""
+    var isActive = ""
+}
--- Hat POS/ViewController.swift
+++ Hat POS/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, HomeModelDelegate {
+    
+    var homeModel = HomeModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        homeModel.getItems()
+        homeModel.delegate = self
         // Do any additional setup after loading the view.
     }
-
+    
+    func itemsDownloaded(inventoryItems: [Inventory]) {
+        
+    }
 
 }
 
