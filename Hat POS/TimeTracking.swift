f751f3dbba693271cb41295ad463ad545ac26087
Levi Knight <KnightL001@mymail.mssu.edu>
2021-05-06 09:17:58 -0500

    Back to where I was in no time.

---
+++ Hat POS/TimeTracking.swift
@@ -0,0 +1,20 @@
+//
+//  TimeTracking.swift
+//  Hat POS
+//
+//  Created by Levi Knight on 5/6/21.
+//
+
+import UIKit
+
+class TimeTracking: UIViewController {
+    
+    @IBOutlet var logo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logo.layer.cornerRadius = 40
+        // Do any additional setup after loading the view.
+    }
+
+}
